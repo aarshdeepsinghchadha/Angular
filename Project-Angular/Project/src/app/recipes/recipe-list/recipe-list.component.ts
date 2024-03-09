5d4924afa0196409fc18b27d98d94bb600a61d9d
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-03-09 15:58:25 +0530

    ViewChild ,Input and Output added to the project also added navbar

--- Project-Angular/Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project-Angular/Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe} from '../recipe.model';
 
 @Component({
@@ -9,7 +9,13 @@ import { Recipe} from '../recipe.model';
 export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe','This is simply a test','https://www.foodandwine.com/thmb/oG2fM9FH1gsuwlcHKjEyi_q1W_Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-beef-chili-recipe-FT-BLOG1019-34a65696ed73447caa2668df7a101060.jpg'),
-    new Recipe('A Test Recipe','This is simply a test','https://www.foodandwine.com/thmb/oG2fM9FH1gsuwlcHKjEyi_q1W_Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-beef-chili-recipe-FT-BLOG1019-34a65696ed73447caa2668df7a101060.jpg')
+    new Recipe('Another Test Recipe','This is simply a test','https://www.foodandwine.com/thmb/oG2fM9FH1gsuwlcHKjEyi_q1W_Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-beef-chili-recipe-FT-BLOG1019-34a65696ed73447caa2668df7a101060.jpg')
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 
 }
--- Project-Angular/Project/src/app/shopping-list/shopping-list.component.ts
+++ Project-Angular/Project/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ingredients } from '../shared/ingredient.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,8 +7,13 @@ import { ingredients } from '../shared/ingredient.model';
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
-  ingredients: ingredients[] = [
-    new ingredients('Apples',5),
-    new ingredients('Tomatoes',10)
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatoes',10)
   ];
+
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+  }
 }
+ 
