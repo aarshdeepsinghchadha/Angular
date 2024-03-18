b3d7523837f8974ee61c57ffac4b0a4e5ab47881
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-03-18 09:58:37 +0530

    added services to the recipe-project

--- Project-Angular/Project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project-Angular/Project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe} from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://www.foodandwine.com/thmb/oG2fM9FH1gsuwlcHKjEyi_q1W_Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-beef-chili-recipe-FT-BLOG1019-34a65696ed73447caa2668df7a101060.jpg'),
-    new Recipe('Another Test Recipe','This is simply a test','https://www.foodandwine.com/thmb/oG2fM9FH1gsuwlcHKjEyi_q1W_Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-beef-chili-recipe-FT-BLOG1019-34a65696ed73447caa2668df7a101060.jpg')
-  ];
+export class RecipeListComponent implements OnInit{
+    
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  constructor(private recipeService: RecipeService){}
 
-  onRecipeSelected(recipe: Recipe){
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipe();
   }
+
+  recipes: Recipe[];
 
 }
---
+++ Project-Angular/Project/src/app/recipes/recipe.service.ts
@@ -0,0 +1,33 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService{
+
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe','This is simply a test','https://www.foodandwine.com/thmb/oG2fM9FH1gsuwlcHKjEyi_q1W_Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-beef-chili-recipe-FT-BLOG1019-34a65696ed73447caa2668df7a101060.jpg'
+        , [
+          new Ingredient('Meat',1),
+          new Ingredient('French Fries',20)
+        ]),
+        new Recipe('Another Test Recipe','This is simply a test','https://www.foodandwine.com/thmb/oG2fM9FH1gsuwlcHKjEyi_q1W_Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-beef-chili-recipe-FT-BLOG1019-34a65696ed73447caa2668df7a101060.jpg',
+        [
+          new Ingredient('Buns',2),
+          new Ingredient('Tomato',1)
+        ])
+      ];
+
+      constructor(private shoppingListService: ShoppingListService){}
+
+      getRecipe(){
+        return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients:  Ingredient[]){
+        this.shoppingListService.addIngredients(ingredients);
+      }
+}
