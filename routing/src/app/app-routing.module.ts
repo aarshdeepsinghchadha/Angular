a2f4b4eb0a2f950ca4909dde505ae2607af9a544
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-03-25 15:58:19 +0530

    added AuthGuard

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -6,13 +6,19 @@ import { ServerComponent } from "./servers/server/server.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "./auth-guard.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'users', component: UserComponent,children:[
       { path: ':id/:name', component: UserComponent }
     ] },
-    { path: 'servers', component: ServersComponent, children: [
+    { 
+      path: 'servers',
+      // canActivate: [AuthGuard] ,
+      canActivateChild: [AuthGuard],
+      component: ServersComponent, 
+      children: [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent}
     ] },
