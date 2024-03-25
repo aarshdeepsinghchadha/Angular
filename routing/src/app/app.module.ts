6a9a1efe877289322431417ae2b0468b7f47b296
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-03-25 14:51:40 +0530

    routing

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -15,7 +15,10 @@ import { RouterModule, Routes } from '@angular/router';
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UserComponent },
-  { path: 'servers', component: ServersComponent }
+  { path: 'users/:id/:name', component: UserComponent },
+  { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id', component: ServerComponent },
+  { path: 'servers/:id/edit', component: EditServerComponent},
 
 ];
 
