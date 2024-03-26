1b9796eed4a04e298a2820f8539d401b4afdaea3
Aarshdeep Singh Chadha <ascnyc29@gmail.com>
2024-03-26 17:16:14 +0530

    adding subject to shoppinglist and cleaning code in the recipe

--- Project-Angular/Project/src/app/recipes/recipe.service.ts
+++ Project-Angular/Project/src/app/recipes/recipe.service.ts
@@ -1,12 +1,11 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+
 @Injectable()
 export class RecipeService{
-
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe','This is simply a test','https://www.foodandwine.com/thmb/oG2fM9FH1gsuwlcHKjEyi_q1W_Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/classic-beef-chili-recipe-FT-BLOG1019-34a65696ed73447caa2668df7a101060.jpg'
